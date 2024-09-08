cc0e6f8fda3d6fd258f951b03befbc8af8f2d554
bamjun <khy5116@naver.com>
2024-09-08 21:41:18 +0900

    print page_source when error

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import time
 
+import requests
 from fastapi import FastAPI, Query
 from pydantic import BaseModel
 from selenium import webdriver
@@ -10,52 +11,49 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from seleniumwire import webdriver  # Selenium 대신 seleniumwire 사용
-import requests
+
 app = FastAPI()
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello from FastAPI on Koyeb!"}
 
 
-#http://localhost:8000/get-title?user=myUser&password=myPass
+# http://localhost:8000/get-title?user=myUser&password=myPass
 @app.get("/get-title")
 async def get_page_title(user: str = "user", password: str = "password"):
     if user == "user":
         return {"error": "Invalid"}
-    
+
     user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"
 
     # Chrome 옵션 설정
     chrome_options = Options()
 
     chrome_options.add_argument("--incognito")  # 시크릿 모드 추가
-    chrome_options.add_argument('--ignore-certificate-errors')  # 인증서 오류 무시
-    chrome_options.add_argument('--ignore-ssl-errors')  # SSL 오류 무시
+    chrome_options.add_argument("--ignore-certificate-errors")  # 인증서 오류 무시
+    chrome_options.add_argument("--ignore-ssl-errors")  # SSL 오류 무시
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument(f"User-Agent={user_agent}")
 
     # Initialize the WebDriver (Chrome)
-    driver = webdriver.Chrome(seleniumwire_options={'verify_ssl': False}, options=chrome_options)
+    driver = webdriver.Chrome(seleniumwire_options={"verify_ssl": False}, options=chrome_options)
 
     try:
         # Open a website (example: Discord)
         driver.get("https://discord.com/login")
 
         # 명시적 대기로 페이지 로딩 완료 및 로그인 폼 대기
-        email_input = WebDriverWait(driver, 50).until(
-            EC.presence_of_element_located((By.NAME, "email"))
-        )
-        password_input = WebDriverWait(driver, 50).until(
-            EC.presence_of_element_located((By.NAME, "password"))
-        )
+        email_input = WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.NAME, "email")))
+        password_input = WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.NAME, "password")))
 
         # 아이디와 비밀번호 입력
         email_input.send_keys(user)
         password_input.send_keys(password)
-        
+
         time.sleep(20)
         # 로그인 버튼 클릭
         login_button = WebDriverWait(driver, 20).until(
@@ -72,25 +70,27 @@ async def get_page_title(user: str = "user", password: str = "password"):
         # 네트워크 요청 중에서 헤더가 있는 모든 요청을 출력
         authorization_key = None
         for request in driver.requests:
-            if 'Authorization' in request.headers:
-                authorization_key = request.headers['Authorization']
+            if "Authorization" in request.headers:
+                authorization_key = request.headers["Authorization"]
                 break
 
     except Exception as e:
         # 오류가 발생했을 때 스크린샷 찍기
         error_screenshot_path = "error_page.png"
         driver.save_screenshot(error_screenshot_path)
+
+        # 현재 페이지의 전체 HTML 가져오기
+        page_source = driver.page_source
+
         driver.quit()
-        return {"error": str(e), "screenshot": error_screenshot_path}
-    
+        return {"error": str(e), "screenshot": error_screenshot_path, "page_source": page_source}
+
     finally:
         # Close the WebDriver
         driver.quit()
 
     # Return the authorization key as a response
     return {"Authorization": authorization_key, "screenshot": screenshot_path}
-
-
 
     # POST /send-message?password=YOUR_DISCORD_AUTH_TOKEN&url=https://discord.com/api/v9/channels/NEW_CHANNEL_ID/messages HTTP/1.1
     # Host: localhost:8000
@@ -99,33 +99,31 @@ async def get_page_title(user: str = "user", password: str = "password"):
     #     "content": "This is a test message with query params and body"
     # }
 
+
 # 요청 바디로 받을 데이터 모델 정의
 class MessageContent(BaseModel):
     content: str
+
 
 # Discord 메시지를 보내는 함수
 @app.post("/send-message")
 async def send_message(
     payload: MessageContent,  # 바디로 받는 content
     password: str = Query("password"),  # 쿼리 파라미터로 받는 password
-    url: str = Query("https://discord.com/api/v9/channels/384597925134860289/messages")
+    url: str = Query("https://discord.com/api/v9/channels/384597925134860289/messages"),
 ):
     # Discord 채널 URL
     # url = "https://discord.com/api/v9/channels/384597925134860289/messages"
-    
+
     # Discord로 보낼 메시지 내용
-    data = {
-        "content": payload.content
-    }
-    
+    data = {"content": payload.content}
+
     # Authorization 헤더 설정
-    headers = {
-        "Authorization": password
-    }
-    
+    headers = {"Authorization": password}
+
     # Discord로 요청 보내기
     res = requests.post(url, json=data, headers=headers)
-    
+
     # 상태 코드에 따른 반환 메시지
     if res.status_code == 200:
         return {"message": "send message success"}
